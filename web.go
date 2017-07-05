d7a03f811b23385c3c2b9ed22d8ee06abab223af
t3k <kn_anoop@hotmail.com>
2017-07-05 03:58:11 +0000

    web sample

---
+++ web.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+  "os"
+)
+
+// func handler ...
+func handler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+
+func main() {
+  http.HandleFunc("/", handler)
+  http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+}
